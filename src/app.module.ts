2aafd1e6c485812ab617e7420a4d739891c9ba05
halki117 <s.halki0203@icloud.com>
2023-05-14 09:10:44 +0900

    認証サービス(auth.service , user.service)の作成

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,10 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { AppDataSource } from './data-source'; // 追加
 import { TypeOrmModule } from '@nestjs/typeorm'; // 追加
+import { AuthService } from './auth/auth.service';
+import { UserService } from './user/user.service';
 
 @Module({
   imports: [TypeOrmModule.forRoot(AppDataSource.options)],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, AuthService, UserService],
 })
 export class AppModule {}
