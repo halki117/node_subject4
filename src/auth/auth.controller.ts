184a6dba5782ec679a62eeff22241b3cee52ad18
halki117 <s.halki0203@icloud.com>
2023-05-15 18:59:05 +0900

    新規登録画面、ログイン画面view（叩き台）作成

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Request, Post, UseGuards, Get, Res } from '@nestjs/common';
+import { Controller, Request, Post, UseGuards, Get, Res, Render  } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { LocalAuthGuard } from './LocalAuth.guard';
 import { JwtAuthGuard } from './JwtAuth.guard';
@@ -8,7 +8,11 @@ import { Response } from 'express';
 @Controller('auth')
 export class AuthController {
   constructor(private authService: AuthService){}
-
+  
+  @Get('login')
+  @Render('auth/login')
+  loginView() {}
+  
   @UseGuards(LocalAuthGuard)
   @Post('login')
   async login(@Request() req) {
@@ -21,6 +25,10 @@ export class AuthController {
     // res.status(HttpStatus.OK).send();
     return this.authService.logout(res);
   }
+
+  @Get('signup')
+  @Render('auth/signup')
+  signupView() {}
 
   @Post('signup')
   async signUp(@Request() req) {
