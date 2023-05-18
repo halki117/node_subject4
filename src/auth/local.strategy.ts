96296480218bca4af32f46c398d17e0c73b6ac3b
halki117 <s.halki0203@icloud.com>
2023-05-18 19:10:23 +0900

    lacalストラテジー編集（emailとpasswordでログインする仕様に変更）

--- src/auth/local.strategy.ts
+++ src/auth/local.strategy.ts
@@ -6,10 +6,13 @@ import { AuthService } from './auth.service';
 @Injectable()
 export class LocalStrategy extends PassportStrategy(Strategy) {
   constructor(private authService: AuthService) {
-    super();
+    super({
+      usernameField: 'email',
+      passwordField: 'password',
+    });
   }
 
-  async validate(name: string, email: string, password: string): Promise<any> {
+  async validate(email: string, password: string): Promise<any> {
     const user = await this.authService.validateUser(email, password);
     if (!user) {
       throw new UnauthorizedException();
