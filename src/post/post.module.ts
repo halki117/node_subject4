9de13241bfad0f8d0dc3e7527067116b3bd0ad5b
halki117 <s.halki0203@icloud.com>
2023-05-24 00:55:51 +0900

    Postモジュールへのimports追記

--- src/post/post.module.ts
+++ src/post/post.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PostService } from './post.service';
 import { PostController } from './post.controller';
+import { Post } from 'src/entities/posts.entity';
+import { TypeOrmModule } from '@nestjs/typeorm';
 
 @Module({
   providers: [PostService],
+  imports: [TypeOrmModule.forFeature([Post])],
   controllers: [PostController]
 })
 export class PostModule {}
