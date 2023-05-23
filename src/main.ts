da1840c06c49dff672b929f9ace8632b64869a8c
halki117 <s.halki0203@icloud.com>
2023-05-24 01:12:32 +0900

    DTOを使ったバリデーションを有効化

--- src/main.ts
+++ src/main.ts
@@ -3,12 +3,13 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 import { join } from 'path';
 import { AppModule } from './app.module';
 import * as cookieParser from 'cookie-parser';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(
     AppModule,
   );
-
+  app.useGlobalPipes(new ValidationPipe());
   app.useStaticAssets(join(__dirname, '..', 'public'));
   app.setBaseViewsDir(join(__dirname, '..', 'views'));
   app.use(cookieParser());
