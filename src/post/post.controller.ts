5c111017443b713d5efcaefe22cc960d242835e2
halki117 <s.halki0203@icloud.com>
2023-05-24 20:26:56 +0900

    投稿作成機能(create)実装

--- src/post/post.controller.ts
+++ src/post/post.controller.ts
@@ -1,8 +1,12 @@
-import { Controller,Request, Post, UseGuards, Get, Res, Render } from '@nestjs/common';
+import { Controller,Request, Post, UseGuards, Get, Res, Render, Body } from '@nestjs/common';
 import { JwtAuthGuard } from '../auth/JwtAuth.guard';
+import { PostService } from './post.service';
+import { CreatePostDTO } from './post.dto';
+import { Response } from 'express';
 
 @Controller('post')
 export class PostController {
+  constructor(private postService: PostService){}
 
   @UseGuards(JwtAuthGuard)
   @Get('create')
@@ -10,6 +14,18 @@ export class PostController {
   createView(@Request() req) {
     const loginUser = req.user ? req.user.username : null;
     return {loginUser: loginUser};
+  }
+
+  @UseGuards(JwtAuthGuard)
+  @Post('create')
+  async create(
+    @Request() req,
+    @Res() res: Response,
+    @Body() post: CreatePostDTO,
+  ) {
+    const loginUserId = req.user ? req.user.userId : null;
+    await this.postService.create(post, loginUserId);
+    res.redirect('/post/posts');
   }
 
   @UseGuards(JwtAuthGuard)
--- src/post/post.service.ts
+++ src/post/post.service.ts
@@ -1,4 +1,27 @@
 import { Injectable } from '@nestjs/common';
+import { Post } from 'src/entities/posts.entity';
+import { Repository, InsertResult } from 'typeorm';
+import { InjectRepository } from '@nestjs/typeorm';
+import { CreatePostDTO } from './post.dto';
 
 @Injectable()
-export class PostService {}
+export class PostService {
+  constructor(
+    @InjectRepository(Post)
+    private readonly postRepository: Repository<Post>
+  ){}
+
+  async findAll(): Promise<Post[]>  {
+    return await this.postRepository.find();
+  }
+
+  async create(Post: CreatePostDTO, loginUserId: number) {
+    console.log(Post, loginUserId);
+    return await this.postRepository.save({
+      title: Post.title,
+      content: Post.content,
+      userId: loginUserId
+    });
+  }
+
+}
